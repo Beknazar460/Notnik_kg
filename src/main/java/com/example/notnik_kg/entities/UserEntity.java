87b27b1edef5cc002bce64407cec0cc190260d11
Beknazar460 <beknazarabdykalykuulu@gmail.com>
2022-10-29 22:27:47 +0600

    create db

--- src/main/java/com/example/notnik_kg/entities/UserEntity.java
+++ src/main/java/com/example/notnik_kg/entities/UserEntity.java
@@ -1,4 +1,26 @@
 package com.example.notnik_kg.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private LocalDateTime dateOfRegister;
+
 }
