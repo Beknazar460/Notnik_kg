14b88651b44a4d850f5a181a1f1bdc4268ad1298
Beknazar460 <beknazarabdykalykuulu@gmail.com>
2022-10-29 21:53:14 +0600

    create new project

---
+++ src/main/java/com/example/notnik_kg/NotnikKgApplication.java
@@ -0,0 +1,13 @@
+package com.example.notnik_kg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotnikKgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotnikKgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/notnik_kg/NotnikKgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notnik_kg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotnikKgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
