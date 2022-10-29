8fc549db147f5cad3f4bb01f023eeed9d9c18857
Beknazar460 <beknazarabdykalykuulu@gmail.com>
2022-10-30 00:31:53 +0600

    create LaptopEntity and sql file

---
+++ src/main/java/com/example/notnik_kg/entities/LaptopEntity.java
@@ -0,0 +1,30 @@
+package com.example.notnik_kg.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "laptops")
+public class LaptopEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "price")
+    private String price;
+
+    @Column(name = "description")
+    private String description;
+
+
+}
