6d170594a8fdd1e6e2453fb2aede60bfa7927765
Beknazar460 <beknazarabdykalykuulu@gmail.com>
2022-10-29 22:47:09 +0600

    validation

--- src/main/java/com/example/notnik_kg/entities/UserEntity.java
+++ src/main/java/com/example/notnik_kg/entities/UserEntity.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Entity
@@ -15,12 +17,35 @@ import java.time.LocalDateTime;
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Size(min = 1, max = 50, message = "email must be between 1 and 50 characters")
+    @Column(name = "email")
     private String email;
+
+    @NotEmpty(message = "Password should not be empty")
+    @Size(min = 1, max = 50, message = "password must be between 1 and 50 characters")
+    @Column(name = "password")
     private String password;
+
+    @NotEmpty(message = "First name should not be empty")
+    @Size(min = 1, max = 50, message = "first name must be between 1 and 50 characters")
+    @Column(name = "first_name")
     private String firstName;
+
+    @NotEmpty(message = "Last name should not be empty")
+    @Size(min = 1, max = 50, message = "last name must be between 1 and 50 characters")
+    @Column(name = "last_name")
     private String lastName;
+
+    @NotEmpty(message = "Phone number should not be empty")
+    @Size(min = 1, max = 50, message = "phone number must be between 1 and 50 characters")
+    @Column(name = "phone_number")
     private String phoneNumber;
-    private LocalDateTime dateOfRegister;
+
+    @Column(name = "date_of_registration")
+    private LocalDateTime dateOfRegistration;
 
 }
--- src/main/resources/db.migration/V1__CREATE_TABLE_USERS.sql
+++ src/main/resources/db.migration/V1__CREATE_TABLE_USERS.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
     email VARCHAR(50) NOT NULL UNIQUE,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    date_of_register TIMESTAMP NOT NULL,
+    date_of_registration TIMESTAMP NOT NULL,
     password VARCHAR(50) NOT NULL,
     phone_number VARCHAR(50) NOT NULL
 );
