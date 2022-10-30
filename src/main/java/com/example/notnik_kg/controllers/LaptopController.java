598344e7477118c38783a130c316d9566dc93ca8
Diyor <diyorp2@mail.ru>
2022-10-30 12:33:45 +0600

    laptopController add

---
+++ src/main/java/com/example/notnik_kg/controllers/LaptopController.java
@@ -0,0 +1,48 @@
+package com.example.notnik_kg.controllers;
+
+import com.example.notnik_kg.entities.LaptopEntity;
+import com.example.notnik_kg.models.LaptopRequest;
+import com.example.notnik_kg.services.Impl.LaptopServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/laptops")
+public class LaptopController {
+    private final LaptopServiceImpl laptopService;
+
+    @Autowired
+    public LaptopController(LaptopServiceImpl laptopService) {
+        this.laptopService = laptopService;
+    }
+
+    @GetMapping()
+    public List<LaptopEntity> getLaptops(){
+        return laptopService.getLaptops();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getLaptopById(@PathVariable("id") Long id){
+        return laptopService.getLaptopById(id);
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> addLaptop(@RequestBody @Valid LaptopRequest laptopRequest){
+        return laptopService.addLaptop(laptopRequest);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateLaptop(@PathVariable("id") Long id,
+                                          @RequestBody @Valid LaptopRequest laptopRequest){
+        return laptopService.updateLaptop(id, laptopRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteLaptop(@PathVariable("id") Long id){
+        return laptopService.deleteLaptop(id);
+    }
+}
